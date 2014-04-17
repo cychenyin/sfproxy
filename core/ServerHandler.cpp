0572c7293cd51682145ae5ec075b6c90ca1354b1
chenyin <chenyin@ganji.com>
2014-04-17 18:52:34 +0800

    change zk access to be  exclusive

--- core/ServerHandler.cpp
+++ core/ServerHandler.cpp
@@ -29,13 +29,15 @@ private:
 	ClientPool *pool;
 	string *root;
 	string split;
-
+	set<string> *skip_set;
+	apache::thrift::concurrency::Mutex mutex;
 public:
 	ServerHandler(string zkhosts) {
 		root = new string("/soa/services");
 		cache = new RegistryCache();
 		pool = new ClientPool(new ZkClientFactory(zkhosts, cache));
 		split = "/";
+		skip_set = new set<string>();
 	}
 
 	~ServerHandler() {
@@ -51,6 +53,8 @@ public:
 			delete root;
 			root = 0;
 		}
+		delete skip_set;
+		skip_set = 0;
 	}
 
 	void get(std::string& _return, const std::string& serviceName) {
@@ -61,31 +65,41 @@ public:
 		long start = ganji::util::time::GetCurTimeUs();
 		long open = start, get = start, id = start, close = start;
 #endif
-
 		string path = *root + split + serviceName;
 		vector<Registry>* pvector = cache->get(path.c_str());
 		if (pvector == 0 || pvector->size() == 0) { // not hit cache, then update cache
-			ZkClient *client = (ZkClient*) pool->open();
+			// if block, then skip
+			set<string>::iterator found = skip_set->find(serviceName);
+			if (found == skip_set->end()) {
+				mutex.lock();
+				skip_set->insert(serviceName);
+				mutex.unlock();
+				ZkClient *client = (ZkClient*) pool->open();
 #ifdef DEBUG_
-			long open = ganji::util::time::GetCurTimeUs();
+				long open = ganji::util::time::GetCurTimeUs();
 #endif
-			if (client) {
-				client->get_children(path);
-				pvector = cache->get(path.c_str());
+				if (client) {
+					client->get_children(path);
+					pvector = cache->get(path.c_str());
 #ifdef DEBUG_
-				cout << " get cache miss." << endl;
-				long get = ganji::util::time::GetCurTimeUs();
-				id = client->id();
+					cout << " get cache miss." << endl;
+					long get = ganji::util::time::GetCurTimeUs();
+					id = client->id();
 #endif
-				client->close(); // must
-			} else {
-				// TODO ... log it
-				cout << " get error, fail to open zk client. pool exhausted maybe. " << endl;
-				return;
+					mutex.lock();
+					//found = skip_set->find(serviceName);
+					skip_set->erase(found);
+					mutex.unlock();
+					client->close(); // must
+				} else {
+					// TODO ... log it
+					cout << " get error, fail to open zk client. pool exhausted maybe. " << endl;
+					return;
+				}
+#ifdef DEBUG_
+				long close = ganji::util::time::GetCurTimeUs();
+#endif
 			}
-#ifdef DEBUG_
-			long close = ganji::util::time::GetCurTimeUs();
-#endif
 		}
 		if (pvector && pvector->size() > 0) {
 			_return = Registry::to_json_string(*pvector);
@@ -95,8 +109,8 @@ public:
 		cout << " pool total=" << pool->size() << " used=" << pool->used() << " idle=" << pool->idle() << endl;
 		long serial = ganji::util::time::GetCurTimeUs();
 		cout << " client id=" << id << " get total cost=" << DiffMs(serial, start) << " open=" << DiffMs(open, start)
-				<< " get=" << DiffMs(get, open) << " close" << DiffMs(close, get) << " serial=" << DiffMs(serial, close)
-				<< endl;
+		<< " get=" << DiffMs(get, open) << " close" << DiffMs(close, get) << " serial=" << DiffMs(serial, close)
+		<< endl;
 //		cache->dump();
 #endif
 	}
@@ -129,7 +143,7 @@ public:
 		long end = ganji::util::time::GetCurTimeUs(); // CLOCKS_PER_SEC
 		cache->dump();
 		cout << "warm " << names.size() << " proxy cost=" << DiffMs(end, start) << "ms. open client cost="
-				<< DiffMs(open, start) << endl;
+		<< DiffMs(open, start) << endl;
 #endif
 
 	}
