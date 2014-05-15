bfcfc9bbd37a4521dc039ac3a5c70a6f043841cd
陈引 <chenyin@yz-kvm-cy-02.corp.ganji.com>
2014-05-15 14:40:14 +0800

    print exception

--- py/rrsend.py
+++ py/rrsend.py
@@ -56,6 +56,15 @@ class Stat:
 
 
 servers = [
+"g1-fang-web-01",
+"g1-fang-web-02",
+"g1-fang-web-03",
+"g1-fang-web-05",
+"g1-fang-web-06",
+"g1-fang-web-07",
+"g1-fang-web-08",
+"g1-fang-web-09",
+"g1-fang-web-10",
 "g1-mob-app-01",
 "g1-mob-app-02",
 "g1-mob-app-03",
@@ -69,6 +78,66 @@ servers = [
 "g1-mob-app-11",
 "g1-mob-app-12",
 "g1-mob-app-13",
+"g1-mob-wap-01",
+"g1-mob-wap-02",
+"g1-mob-wap-03",
+"g1-mob-wap-04",
+"g1-mob-wap-05",
+"g1-ms-sta-05",
+"g1-ms-sta-06",
+"g1-ms-sta-07",
+"g1-ms-sta-08",
+"g1-ms-web-01",
+"g1-ms-web-02",
+"g1-ms-web-04",
+"g1-ms-web-05",
+"g1-ms-web-08",
+"g1-ms-web-09",
+"g1-sec-web-01",
+"g1-sec-web-02",
+"g1-sec-web-03",
+"g1-sec-web-04",
+"g1-zp-web-01",
+"g1-zp-web-02",
+"g1-zp-web-03",
+"g1-zp-web-05",
+"g1-zp-web-07",
+"g1-zp-web-08",
+"jxq-ms-sta-01",
+"jxq-ms-sta-02",
+"jxq-ms-sta-03",
+"jxq-ms-sta-04",
+"jxq-ms-sta-05",
+"jxq-ms-sta-06",
+"yz-adm-web-01",
+"yz-adm-web-02",
+"yz-fang-web-02",
+"yz-fang-web-03",
+"yz-fang-web-04",
+"yz-fang-web-05",
+"yz-fang-web-06",
+"yz-fang-web-08",
+"yz-fang-web-09",
+"yz-fang-web-10",
+"yz-fang-web-11",
+"yz-fang-web-12",
+"yz-fang-web-13",
+"yz-fang-web-14",
+"yz-im-web-01",
+"yz-im-web-02",
+"yz-im-web-03",
+"yz-im-web-04",
+"yz-im-web-05",
+"yz-im-web-06",
+"yz-im-web-07",
+"yz-im-web-08",
+"yz-im-web-09",
+"yz-im-web-10",
+"yz-im-web-11",
+"yz-im-web-12",
+"yz-im-web-14",
+"yz-im-web-15",
+"yz-im-web-16",
 "yz-mob-app-01",
 "yz-mob-app-02",
 "yz-mob-app-04",
@@ -80,11 +149,6 @@ servers = [
 "yz-mob-app-11",
 "yz-mob-app-12",
 "yz-mob-app-13",
-"g1-mob-wap-01",
-"g1-mob-wap-02",
-"g1-mob-wap-03",
-"g1-mob-wap-04",
-"g1-mob-wap-05",
 "yz-mob-wap-01",
 "yz-mob-wap-02",
 "yz-mob-wap-03",
@@ -100,6 +164,62 @@ servers = [
 "yz-mob-wap-14",
 "yz-mob-wap-15",
 "yz-mob-wap-16",
+"yz-ms-web-01",
+"yz-ms-web-03",
+"yz-ms-web-08",
+"yz-ms-web-09",
+"yz-ms-web-10",
+"yz-ms-web-18",
+"yz-ms-web-19",
+"yz-ms-web-24",
+"yz-ms-web-31",
+"yz-ms-web-a06",
+"yz-ms-web-a07",
+"yz-ms-web-a09",
+"yz-pay-web-01",
+"yz-pay-web-02",
+"yz-pay-web-03",
+"yz-pay-web-03",
+"yz-psp-web-01",
+"yz-psp-web-02",
+"yz-psp-web-03",
+"yz-sec-web-01",
+"yz-sec-web-02",
+"yz-sec-web-03",
+"yz-sec-web-04",
+"yz-sec-web-05",
+"yz-sec-web-06",
+"yz-sec-web-07",
+"yz-sec-web-09",
+"yz-tg-web-01",
+"yz-tg-web-02",
+"yz-tg-web-03",
+"yz-tg-web-04",
+"yz-tg-web-05",
+"yz-tg-web-06",
+"yz-tg-web-07",
+"yz-tg-web-08",
+"yz-tg-web-09",
+"yz-zp-web-01",
+"yz-zp-web-02",
+"yz-zp-web-03",
+"yz-zp-web-04",
+"yz-zp-web-05",
+"yz-zp-web-06",
+"yz-zp-web-07",
+"yz-zp-web-08",
+"yz-zp-web-09",
+"yz-zp-web-10",
+"yz-zp-web-11",
+"yz-zp-web-12",
+"yz-fw-web-01",
+"yz-fw-web-02",
+"yz-fw-web-03",
+"yz-fw-web-04",
+"yz-fw-web-05",
+"yz-fw-web-06",
+"yz-fw-web-07",
+
 ]
 
 stats = {host:Stat(host) for host in servers  }
@@ -120,15 +240,21 @@ flog = open(logFileName, "a+")
 port = 9009
 http = False
 framed = True
+timeout = 100
 pp = pprint.PrettyPrinter(indent = 1)
+round = 0
 while(True):
+	round = round + 1
+	print "round %d" % round
 	for host in servers:
 		stat = stats[host]
 
 		if http:
 			transport = THttpClient.THttpClient(host, port, uri)
+			transport.setTimeout(timeout)
 		else:
 			socket = TSocket.TSocket(host, port)
+			socket.setTimeout(timeout)
 		if framed:
 			transport = TTransport.TFramedTransport(socket)
 		else:
@@ -142,20 +268,20 @@ while(True):
 			client.get("rta.counter.thrift")
 			stat.success()
 		except TApplicationException, e:
-			print "{} TApplicationException {}".format(timestamp_fromunixtime(), e._message)
+			print "{} TApplicationException {} {}".format(timestamp_fromunixtime(), host, e.message)
 			stat.fail()
 		except TException, e:
-			print "{} TException {}".format(timestamp_fromunixtime(), e._message)
+			print "{} TException {} {}".format(timestamp_fromunixtime(), host, e.message)
 			stat.fail()
 		except Exception, e:
-			print "{} Exception {}: {}".format(timestamp_fromunixtime(), e.errno, e.strerror)
+			print "{} Exception {} {}".format(timestamp_fromunixtime(), host, e) 
 			stat.fail()
 
 		except IOError, e:
-			print "{} IOError {}: {}".format(timestamp_fromunixtime(), e.errno, e.strerror)
+			print "{} IOError {} {}: {} {}".format(timestamp_fromunixtime(), host, e.errno, e.strerror, e)
 			stat.fail()
 		except:
-			print "unknown exception occured."
+			print "unknown exception occured. {}".format(host)
 			stat.fail()
 		finally:
 			end = time.clock() - start
@@ -166,16 +292,13 @@ while(True):
 				flog.write(stat.toString() + "\n")
 			except:
 				print "write log error."
+	flog.flush()
 	if os.path.getsize(logFileName) > logMaxSize:
-		flog.flush()
 		flog.close()
 		logFileName = "burn.log." + timestamp_fromunixtime2()		
 		flog = open(logFileName, "a+")
-	time.sleep(10)
-
+	time.sleep(0.5)
 flog.flush()
 flog.close()
-ferror.flush()
-ferror.close()
 
 
